3e391c62233a9611476acc28cecb82eab669296b
Oyewo Ayooluwa <ayooluwaoyewo@gmail.com>
2018-07-01 22:30:01 +0100

    new

--- app/js/swcontroller/index.js
+++ app/js/swcontroller/index.js
@@ -3,7 +3,7 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
-        .then(() => { console.log("Service Worker Registered"); })
+        .then(reg => { console.log("Service Worker Registered"); })
         .catch(() => {
             console.error('Service Worker could not be registered:');
         });
